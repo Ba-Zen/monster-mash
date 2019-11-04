726da9e86274021aef5122385bafb9912caceb35
Ba-Zen <bazenatlas@gmail.com>
2019-11-04 13:32:50 -0500

    Updates

---
+++ precache-manifest.997d494db5267fd2879e5b69ce351bea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b40ed2dfc89bb4bb74e01b4d134f031",
+    "url": "/monster-mash/index.html"
+  },
+  {
+    "revision": "87ae74d151e0fb75d898",
+    "url": "/monster-mash/static/css/main.153bd4cf.chunk.css"
+  },
+  {
+    "revision": "6cd62c93ea17354b5bc7",
+    "url": "/monster-mash/static/js/2.7a97efe7.chunk.js"
+  },
+  {
+    "revision": "87ae74d151e0fb75d898",
+    "url": "/monster-mash/static/js/main.a0be2d91.chunk.js"
+  },
+  {
+    "revision": "ecca85f4bf2af67fc585",
+    "url": "/monster-mash/static/js/runtime-main.d8c91ede.js"
+  }
+]);
